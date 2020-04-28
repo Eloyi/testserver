efd19a9d78757cc7ad6684ef91299ce4c8f83912
eloyqin <qinhuang690@gmail.com>
2020-04-28 09:02:59 +0800

    support upload file

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # 静态服务器
 [TOC]
 ## 启动
+npm install --save formidable@1.2.2
+
 node server.js
 启动成功后，可以访问目录下的文件
 http://127.0.0.1:8887/apk/app-dev-debug.apk
--- server.js
+++ server.js
@@ -1,57 +1,89 @@
-const http = require('http');  //引入node http模块
-const fs = require('fs');    //引入node 文件读写fs模块
-const path = require('path');   //引入 node path模块
-const url = require('url');     //引入  url  模块
- 
- 
+const http = require('http'); //引入node http模块
+const fs = require('fs'); //引入node 文件读写fs模块
+const path = require('path'); //引入 node path模块
+const url = require('url'); //引入  url  模块
+
+var formidable = require('formidable');
+
+
 const resMime = require('./js/resMime.js'); //引入自定义模块，模块主要处理响应头
- 
- 
-const server = http.createServer((req,res)=>{   //http.createServer()创建服务器
-    
-    let pathName = url.parse(req.url).pathname;  //获取req.url,并且转换请求的路径
- 
-    // console.log(url.parse('index.html/12323123'));  会得到 index.html
- 
-    console.log(pathName); //控制台打印看能不能得到请求路径
- 
-    if(pathName=='/'){  //如果请求为空或“/”，则把请求路径改为index.html
-    	pathName = '/index.html';
+
+
+const server = http.createServer((req, res) => {
+
+        console.log(req.method);
+    if (req.method == 'POST') {
+
+        // if (!isFormData(req)) {
+        //     res.statusCode = 400
+        //     return
+        // }
+        var form = new formidable.IncomingForm()
+        form.uploadDir = path.join(__dirname, 'upload')
+        form.keepExtensions = true
+
+        form.on('field', (field, value) => {
+            console.log(field)
+            console.log(value)
+        })
+        form.on('end', () => {
+            res.end('上传完成!')
+        })
+
+        form.parse(req)
+
+    } else {
+        //http.createServer()创建服务器
+
+        let pathName = url.parse(req.url).pathname; //获取req.url,并且转换请求的路径
+
+        // console.log(url.parse('index.html/12323123'));  会得到 index.html
+
+        console.log(pathName); //控制台打印看能不能得到请求路径
+
+        if (pathName == '/') { //如果请求为空或“/”，则把请求路径改为index.html
+            pathName = '/index.html';
+        }
+
+        if (pathName == '/favicon.ico') { //如果请求为“/favicon.ico”图标,则直接结束不处理
+            res.end();
+        }
+
+        if (pathName != '/favicon.ico') {
+            //这里正式写处理逻辑
+            console.log('开始读取文件');
+            let extName = path.extname(pathName); //拿到扩展名（比如说：'index.html' => '.html'）
+            //下面用到 fs 去读取客户端请求的文件，path.join拼接成绝对路径
+            fs.readFile(path.join(__dirname, pathName), function(err, data) { //绝对路径，path.jion(__dirname,pathName)
+                if (err) {
+                    //没有找到的话，就走ERR，转去加载404页面
+                    console.log(err);
+                    fs.readFile(path.join(__dirname, `404.html`), (err, data) => {
+
+                        //res.writeHead()向客户端返回响应头，状态码404，因为返回的是自己写的404页面，所以响应头应该为"Content-Type":"text/html;chartset='utf8'" 
+
+                        res.writeHead(404, { "Content-Type": "text/html;chartset='utf8'" })
+                        res.write(data); //把文件写回客户端
+                        res.end(); //结束
+                    })
+                } else {
+                    //这里fs找到了请求资源
+                    console.log(data) //打印看看资源的内容
+                    var mime = resMime.getMime(fs, extName); //调用外部模块resMime,这个模块我是用来，请求的文件后缀名转换成mime标准的响应头Content-Type类型（比如说：'.css'=>'text/css','.js'=>'text/javascript'）
+                    res.writeHead(200, { "Content-Length": Buffer.byteLength(data), "Content-Type": `${mime};chartset='utf8'` });
+                    res.write(data); //把文件写回客户端
+                    res.end(); //结束
+                }
+            });
+
+        }
     }
- 
-    if(pathName=='/favicon.ico'){  //如果请求为“/favicon.ico”图标,则直接结束不处理
-    	res.end();
-    }
- 
-    if(pathName!= '/favicon.ico'){  
-        //这里正式写处理逻辑
-        console.log('开始读取文件');
-        let extName = path.extname(pathName);//拿到扩展名（比如说：'index.html' => '.html'）
-        //下面用到 fs 去读取客户端请求的文件，path.join拼接成绝对路径
-         fs.readFile(path.join(__dirname,pathName), function(err,data){   //绝对路径，path.jion(__dirname,pathName)
-         	if(err){    
-                //没有找到的话，就走ERR，转去加载404页面
-         		console.log(err);
-         		fs.readFile(path.join(__dirname,`404.html`),(err,data)=>{
- 
-                    //res.writeHead()向客户端返回响应头，状态码404，因为返回的是自己写的404页面，所以响应头应该为"Content-Type":"text/html;chartset='utf8'" 
-                    
-         			res.writeHead(404,{"Content-Type":"text/html;chartset='utf8'"})
-         			res.write(data); //把文件写回客户端
-         			res.end();   //结束
-         		})
-         	}else{
-                //这里fs找到了请求资源
-         		console.log(data) //打印看看资源的内容
-         		var mime = resMime.getMime(fs,extName);  //调用外部模块resMime,这个模块我是用来，请求的文件后缀名转换成mime标准的响应头Content-Type类型（比如说：'.css'=>'text/css','.js'=>'text/javascript'）
-         		res.writeHead(200,{"Content-Length": Buffer.byteLength(data),"Content-Type":`${mime};chartset='utf8'`});
-         		res.write(data); //把文件写回客户端
-         		res.end();   //结束
-         	}
-         });
- 
-    }
-    
+
 });
- 
-server.listen('8887');    //让服务器跑在8887端口
+
+function isFormData(req) {
+  let type = req.headers['content-type'] || ''
+  return type.includes('multipart/form-data')
+}
+
+server.listen('8887'); //让服务器跑在8887端口
